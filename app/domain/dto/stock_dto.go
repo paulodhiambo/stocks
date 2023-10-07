40e0e254f12dc27f189204e34e8eb6de1dd4c5af
Odhiambo Paul <paulodhiambo962@gmail.com>
2023-10-07 23:25:00 +0300

    project configuration

---
+++ app/domain/dto/stock_dto.go
@@ -0,0 +1,24 @@
+package dto
+
+type StockDto struct {
+	TradeId       int     `json:"trade_id"`
+	StockSymbol   string  `json:"stock_symbol"`
+	TradeDate     string  `json:"trade_date"`
+	TradeTime     string  `json:"trade_time"`
+	TradePrice    float64 `json:"trade_price"`
+	TradeVolume   int     `json:"trade_volume"`
+	BuyerId       int     `json:"buyer_id"`
+	SellerId      int     `json:"seller_id"`
+	BuyerName     string  `json:"buyer_name"`
+	SellerName    string  `json:"seller_name"`
+	BuyerCountry  string  `json:"buyer_country"`
+	SellerCountry string  `json:"seller_country"`
+	BuyerEmail    string  `json:"buyer_email"`
+	SellerEmail   string  `json:"seller_email"`
+	BuyerPhone    string  `json:"buyer_phone"`
+	SellerPhone   string  `json:"seller_phone"`
+	BuyerAddress  string  `json:"buyer_address"`
+	SellerAddress string  `json:"seller_address"`
+	BuyerCity     string  `json:"buyer_city"`
+	SellerCity    string  `json:"seller_city"`
+}
---
+++ tests/controller/stock_controller_test.go
@@ -0,0 +1,23 @@
+package controller
+
+import "testing"
+
+func TestGetAllStock(t *testing.T) {
+
+}
+
+func TestGetStockByID(t *testing.T) {
+
+}
+
+func TestDeleteAStock(t *testing.T) {
+
+}
+
+func TestCreateAStock(t *testing.T) {
+
+}
+
+func TestUpdateAStock(t *testing.T) {
+
+}
